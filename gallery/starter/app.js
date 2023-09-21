f946afade86fb7aee03a067ff50b08c9435563ff
Matthew Jenner <matthewjenner@hotmail.com>
2023-09-21 23:24:31 +0100

    modal set up ready for functionality

--- gallery/starter/app.js
+++ gallery/starter/app.js
@@ -8,7 +8,9 @@ function getElement(selection) {
   );
 }
 
+
 function Gallery(element){
+  this.container = element
   this.list = [...element.querySelectorAll('.img')]
   
 // target
@@ -17,8 +19,21 @@ this.modalImg = getElement('.main-img')
 this.modalImages = getElement('.modal-images')
 
 this.closeBtn = getElement('.close-btn')
-this.nextBtn = getElement('.close-btn')
-this.prevBtn = getElement('.close-btn')
+this.nextBtn = getElement('.next-btn')
+this.prevBtn = getElement('.prev-btn')
+//bind functions
+//this.openModal = this.openModal.bind(this)
+this.container.addEventListener('click', function(e) {
+  console.log(this)
+  this.openModal()
+}.bind(this))
+}
+
+
+Gallery.prototype.openModal = function() {
+  console.log(this)
+  console.log('open modal')
+  this.modal.classList.add('open')
 }
 
 const nature = new Gallery(getElement('.nature'))
