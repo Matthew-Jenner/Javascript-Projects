931595a8ab7e381e3061a4c6df0796f534d54b19
Matthew Jenner <matthewjenner@hotmail.com>
2023-09-22 12:49:47 +0100

    close modal functionality added

--- gallery/starter/app.js
+++ gallery/starter/app.js
@@ -26,6 +26,9 @@ this.prevBtn = getElement('.prev-btn')
 //let self = this
 //bind functions
 //this.openModal = this.openModal.bind(this)
+this.closeModal = this.closeModal.bind(this)
+this.nextImage = this.nextImage.bind(this)
+this.prevImage = this.prevImage.bind(this)
 //container event
 this.container.addEventListener('click', function(e) {
   // self.openModal()
@@ -43,11 +46,23 @@ Gallery.prototype.openModal = function(selectedImage, list) {
    return `<img src="${image.src}" title="${image.title}" data-id="${image.dataset.id}" class="${selectedImage.dataset.id ===image.dataset.id?'modal-img selected' : 'modal-img'}" />`
   }).join('')
   this.modal.classList.add('open')
+  this.closeBtn.addEventListener('click', this.closeModal)
+  this.nextBtn.addEventListener('click', this.nextImage)
+  this.prevBtn.addEventListener('click', this.prevImage)
 }
 Gallery.prototype.setMainImage = function(selectedImage){
   this.modalImg.src = selectedImage.src
   this.imageName.textContent = selectedImage.title
 }
+Gallery.prototype.closeModal = function(){
+  this.modal.classList.remove('open')
+  this.closeBtn.remove.addEventListener('click', this.closeModal)
+  this.nextBtn.remove.addEventListener('click', this.nextImage)
+  this.prevBtn.remove.addEventListener('click', this.prevImage)
+}
+
+Gallery.prototype.nextImage = function() {}
+Gallery.prototype.prevImage = function() {}
 
 const nature = new Gallery(getElement('.nature'))
 const city = new Gallery(getElement('.city'))
