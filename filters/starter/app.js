6e98f9f366a96f6b26798f50c254b13d55c7a5f7
Matthew Jenner <matthewjenner@hotmail.com>
2023-10-02 13:52:08 +0100

    Empty array return message

--- filters/starter/app.js
+++ filters/starter/app.js
@@ -3,7 +3,11 @@ let filteredProducts = [...products];
 const productsContainer = document.querySelector(".products-container");
 
 const displayProducts = () => {
-	// if statement
+	if(filteredProducts.length < 1) {
+        productsContainer.innerHTML = `<h6> Sorry, me no likey </h6>`
+        return  
+        // without the return nothing happens
+    }
 
 	productsContainer.innerHTML = filteredProducts
 		.map(({ id, title, image, price }) => {
