3b3203f04dff2bbf35a36fef7614c4e4f31970c0
Matthew Jenner <matthewjenner@hotmail.com>
2023-10-12 18:10:00 +0100

    Data accessed, errors caught and loading spinner

--- products/starter/app.js
+++ products/starter/app.js
@@ -1,1 +1,25 @@
-console.log('products starter');
+const url = "https://course-api.com/javascript-store-products";
+
+const productsDom = document.querySelector(".products-center");
+
+const fetchProducts = async () => {
+	productsDom.innerHTML = `<div class="loading"></div>`;
+	try {
+		const resp = await fetch(url);
+		const data = await resp.json();
+		return data;
+	} catch (error) {
+		productsDom.innerHTML = `<p class="error">there was an error</p>`;
+	}
+};
+
+const displayProducts = (list) => {
+	console.log(list);
+};
+const start = async () => {
+	const data = await fetchProducts()
+    displayProducts(data)
+};
+start();
+
+fetchProducts();
