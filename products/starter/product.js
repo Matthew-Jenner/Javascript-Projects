8129b21a91e50e327283618fa469ce92bb9f260f
Matthew Jenner <matthewjenner@hotmail.com>
2023-10-15 10:46:22 +0100

    Single products fetched dynamiclly

--- products/starter/product.js
+++ products/starter/product.js
@@ -1,9 +1,19 @@
-const productsDom = document.querySelector(".product");
+const productDom = document.querySelector(".product");
 const url =
-	"https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog";
+	"https://course-api.com/javascript-store-single-product";
 
 const fetchProduct = async () => {
-	return "product";
+	try {
+		productDom.innerHTML = '<h4 class="product-loading">Lodi dodi....</h4>';
+       const params = new URLSearchParams(window.location.search)
+       const id = params.get('id')
+       console.log(id)
+        const response = await fetch(`${url}?id=${id}`)
+        const data = await response.json()
+        return data
+	} catch (error) {
+        productDom.innerHTML = '<p class="error"> No luck my G.</p>';
+    }
 };
 
 const displayProduct = (product) => {
