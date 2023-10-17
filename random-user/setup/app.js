15652d7d8edb6cd18dc3157cac490350297baf0e
Matthew Jenner <matthewjenner@hotmail.com>
2023-10-17 10:22:39 +0100

    elements selected

--- random-user/setup/app.js
+++ random-user/setup/app.js
@@ -1,1 +1,16 @@
-const url = 'https://randomuser.me/api/' 
+import get from "./utils/getElement.js";
+
+const url = "https://randomuser.me/api/";
+
+const img = get(".user-img");
+const title = get(".user-title");
+const value = get(".user-value");
+const btn = get(".btn");
+const btns = [...document.querySelectorAll('.icon')]
+
+const showUser = () => {
+    console.log('hello world');
+}
+
+window.addEventListener('DOMContentLoaded', showUser)
+btn.addEventListener('click', showUser)
