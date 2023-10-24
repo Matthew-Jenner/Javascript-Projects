5a568d22046a733fe8785e045229b7b06934e30d
Matthew Jenner <matthewjenner@hotmail.com>
2023-10-24 23:57:49 +0100

    debug - found and fixed display issue with toggle loading module

--- cocktails/setup/src/toggleLoading.js
+++ cocktails/setup/src/toggleLoading.js
@@ -3,8 +3,8 @@ import get from "./getElement.js";
 const loading = get(".loading");
 
 export const showLoading = () => {
-    loading.classList.remove('hide-loading')
-}
+	loading.classList.remove("hide-loading");
+};
 export const hideLoading = () => {
-    loading.classList.remove('hide-loading')
-}
+	loading.classList.add("hide-loading");
+};
