af777193d91cd9ec420fdb49429afd535a9f21d5
Matthew Jenner <matthewjenner@hotmail.com>
2023-10-26 23:10:19 +0100

    Displayed single drink using dynamic id and accessing ingredients and description

--- cocktails/setup/src/setDrink.js
+++ cocktails/setup/src/setDrink.js
@@ -1,9 +1,11 @@
 const setDrink = (section) => {
 	section.addEventListener("click", function (e) {
 		// e.preventDefault();
-		const id = e.target.parentElement.dataSet.id;
+		const id = e.target.parentElement.dataset.id;
         // JSON.stringify JSON.parse
+        if(id) {
         localStorage.setItem('drink', id)
+        }
 	});
 };
 export default setDrink;
