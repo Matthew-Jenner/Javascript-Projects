dd305f56c47d92fe2124fd85c01fe855bfb7b25b
Matthew Jenner <matthewjenner@hotmail.com>
2023-10-29 10:24:49 +0000

    accessed people array and dynamically added to project

---
+++ slider/setup/app.js
@@ -0,0 +1,25 @@
+import people from "./data.js";
+
+const container = document.querySelector(".slide-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+// set slides
+container.innerHTML = people.map((person, slideIndex) => {
+	const { img, name, job, text } = person;
+	// more logic later
+	return `<article class="slide">
+<img
+    src=" ${img}"
+    alt="${name}"
+    class="img" />
+<h4>${name}</h4>
+<p class="title">${job}</p>
+<p class="text">
+    ${text}
+</p>
+<div class="quote-icon">
+    <div class="fas fa-quote-right"></div>
+</div>
+</article>`;
+}).join('');
