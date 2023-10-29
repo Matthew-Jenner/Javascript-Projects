c0566bdb33ba22f29eeb59a3dc3d90dd8e8c3446
Matthew Jenner <matthewjenner@hotmail.com>
2023-10-29 10:56:41 +0000

    end of array returns to first

--- slider/setup/app.js
+++ slider/setup/app.js
@@ -5,17 +5,18 @@ const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
 
 // set slides
-container.innerHTML = people.map((person, slideIndex) => {
-	const { img, name, job, text } = person;
-	// more logic later
-    let position = 'next'
-    if(slideIndex === 0){
-        position = 'active'
-    }
-    if(slideIndex === people.length -1) {
-        position = 'last'
-    }
-	return `<article class="slide ${position}">
+container.innerHTML = people
+	.map((person, slideIndex) => {
+		const { img, name, job, text } = person;
+		// more logic later
+		let position = "next";
+		if (slideIndex === 0) {
+			position = "active";
+		}
+		if (slideIndex === people.length - 1) {
+			position = "last";
+		}
+		return `<article class="slide ${position}">
 <img
     src=" ${img}"
     alt="${name}"
@@ -29,25 +30,28 @@ container.innerHTML = people.map((person, slideIndex) => {
     <div class="fas fa-quote-right"></div>
 </div>
 </article>`;
-}).join('');
+	})
+	.join("");
 
-const startSlider = (type) =>{
-  const active = document.querySelector('.active')
-  const last = document.querySelector('.last')
-  let next = active.nextElementSibling
-  active.classList.remove(['active'])
-  last.classList.remove(['last'])
-  next.classList.remove(['next'])
+const startSlider = (type) => {
+	const active = document.querySelector(".active");
+	const last = document.querySelector(".last");
+	let next = active.nextElementSibling;
+    if(!next) {
+        next = container.firstElementChild
+    }
+	active.classList.remove(["active"]);
+	last.classList.remove(["last"]);
+	next.classList.remove(["next"]);
 
-  active.classList.add('last')
-  last.classList.add('next')
-  next.classList.add('active')
-}
+	active.classList.add("last");
+	last.classList.add("next");
+	next.classList.add("active");
+};
 
-
-nextBtn.addEventListener('click', () =>{
-    startSlider()
-})
-prevBtn.addEventListener('click', ()=> {
-    startSlider('prev')
-})
+nextBtn.addEventListener("click", () => {
+	startSlider();
+});
+prevBtn.addEventListener("click", () => {
+	startSlider("prev");
+});
