7f290090d376da578910f698ac43f8be8292af3f
Matthew Jenner <matthewjenner@hotmail.com>
2023-11-04 23:50:36 +0000

    basic formatting

--- stripe-submenus/setup/app.js
+++ stripe-submenus/setup/app.js
@@ -24,41 +24,43 @@ sidebar.innerHTML = sublinks
 		const { links, page } = item;
 		return `<article> <h4> ${page}</h4>
     <div class="sidebar-sublinks">
-    ${links.map((link) => {
-			return `<a href="${link.url}">
+    ${links
+			.map((link) => {
+				return `<a href="${link.url}">
             <i class="${link.icon}"></i>${link.label}
-            </a>`
-		}).join("")}
+            </a>`;
+			})
+			.join("")}
     </div>
     </article>`;
 	})
 	.join("");
 
-    linkBtns.forEach((btn) =>{
-        btn.addEventListener('mouseover', function (e) {
-         const text = e.currentTarget.textContent
-         const tempBtn = e.currentTarget.getBoundingClientRect()
-         const center = (tempBtn.left + tempBtn.right) / 2
-         const bottom = tempBtn.bottom -3
+linkBtns.forEach((btn) => {
+	btn.addEventListener("mouseover", function (e) {
+		const text = e.currentTarget.textContent;
+		const tempBtn = e.currentTarget.getBoundingClientRect();
+		const center = (tempBtn.left + tempBtn.right) / 2;
+		const bottom = tempBtn.bottom - 3;
 
-         const tempPage = sublinks.find(({page}) => page === text)
-        if(tempPage){
-            const {page, links} = tempPage
-            submenu.classList.add('show')
-            submenu.style.left = `${center}px`
-            submenu.style.top = `${bottom}px`
-            submenu.innerHTML = 
-            `<section>
+		const tempPage = sublinks.find(({ page }) => page === text);
+		if (tempPage) {
+			const { page, links } = tempPage;
+			submenu.classList.add("show");
+			submenu.style.left = `${center}px`;
+			submenu.style.top = `${bottom}px`;
+			submenu.innerHTML = `<section>
             <h4>${page}</h4>
             <div class="submenu-center col-2">
-            ${links.map((link) =>{
-                return `<a href="${link.url}">
+            ${links
+							.map((link) => {
+								return `<a href="${link.url}">
                 <i class="${link.icon}"></i> ${link.label}
-                </a>`
-            }).join('')}
+                </a>`;
+							})
+							.join("")}
             </div>
-            </section>`
-
-        }
-        })
-    })
+            </section>`;
+		}
+	});
+});
