33c6e89066e194d3d1dd1de5bf31576cba0a1443
Matthew Jenner <matthewjenner@hotmail.com>
2023-11-08 23:36:21 +0000

    minor refactoring

--- wikipedia/starter/app.js
+++ wikipedia/starter/app.js
@@ -1,48 +1,48 @@
 const url =
-  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
+	"https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=";
 
-const formDOM = document.querySelector('.form')
-const inputDOM = document.querySelector('.form-input')
-const resultsDOM = document.querySelector('.results')
+const formDOM = document.querySelector(".form");
+const inputDOM = document.querySelector(".form-input");
+const resultsDOM = document.querySelector(".results");
 
-formDOM.addEventListener('submit', (e) => {
-e.preventDefault()
-const value = inputDOM.value
-if(!value){
-    resultsDOM.innerHTML = '<div class="error"> Please enter valid search term</div>'
-    return
-}
-fetchPages(value)
-})
+formDOM.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const value = inputDOM.value;
+	if (!value) {
+		resultsDOM.innerHTML =
+			'<div class="error"> Please enter valid search term</div>';
+		return;
+	}
+	fetchPages(value);
+});
 
 const fetchPages = async (searchValue) => {
-    resultsDOM.innerHTML = '<div class="loading"></div>'
-    try {
-        const response = await fetch(`${url}${searchValue}`)
-        const data = await response.json()
-        const results = data.query.search
-        if(results.length < 1) {
-            resultsDOM.innerHTML = 
-            '<div class="error"> no matching results</div>'
-            return
-        }
-        
-    } catch (error) {
-        resultsDOM.innerHTML = '<div class="error"> There was an error...</div>'
-        
-    }
-}
-const renderResults = (list) =>{
-   const cardsList = list.map((item) => {
-    const {title, snippet, pageid} = item
-    return `<a href="'href=http://en.wikipedia.org/?curid=${pageid}'" target="_blank">
+	resultsDOM.innerHTML = '<div class="loading"></div>';
+	try {
+		const response = await fetch(`${url}${searchValue}`);
+		const data = await response.json();
+		const results = data.query.search;
+		if (results.length < 1) {
+			resultsDOM.innerHTML = '<div class="error"> no matching results</div>';
+			return;
+		}
+	} catch (error) {
+		resultsDOM.innerHTML = '<div class="error"> There was an error...</div>';
+	}
+};
+const renderResults = (list) => {
+	const cardsList = list
+		.map((item) => {
+			const { title, snippet, pageid } = item;
+			return `<a href="http://en.wikipedia.org/?curid=${pageid}" target="_blank">
     <h4>${title}</h4>
     <p>
       ${snippet}
     </p>
-  </a>`
-   }).join('')
-   resultsDOM.innerHTML = `<div class="articles">
+  </a>`;
+		})
+		.join("");
+	resultsDOM.innerHTML = `<div class="articles">
    ${cardsList}
-   </div>`
-}
+   </div>`;
+};
