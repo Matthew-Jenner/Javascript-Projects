acb95d316e0adb1d3e05a43405b743d15a6728b1
Matthew Jenner <matthewjenner@hotmail.com>
2023-11-16 23:57:25 +0000

    filter products completed dynamically

--- comfy-store/starter/src/filters/price.js
+++ comfy-store/starter/src/filters/price.js
@@ -13,6 +13,18 @@ const setupPrice = (store) => {
     priceInput.max = maxPrice
     priceInput.min = 0
     priceValue.textContent = `Value : $${maxPrice}`
+
+    priceInput.addEventListener('input', function(){
+        const value = parseInt(priceInput.value)
+        priceValue.textContent = `Value : $${value}`
+        let newStore = store.filter((product) => product.price / 100 <= value)
+        display(newStore, getElement('.products-container'))
+        if(newStore.length < 1) {
+            const products = getElement('.products-container')
+            products.innerHTML = `<h3 class="filter-error">Sorry, no products matched your search</h3>`
+        }
+
+    })
 };
 
 export default setupPrice;
