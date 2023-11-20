578ec36da0baa3b760e299728c621a1aa83c5b4c
Matthew Jenner <matthewjenner@hotmail.com>
2023-11-20 13:41:37 +0000

    add to cart setup

--- comfy-store/starter/src/store.js
+++ comfy-store/starter/src/store.js
@@ -14,5 +14,8 @@ const setupStore = (products) => {
     console.log('updated store', store)
 };
 
-const findProduct = () => {};
+const findProduct = (id) => {
+	let product = store.find((product) => product.id === id)
+	return product
+};
 export { store, setupStore, findProduct };
