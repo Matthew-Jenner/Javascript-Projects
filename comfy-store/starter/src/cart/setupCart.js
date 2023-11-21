913b8a1e4f253849e6789e35a7765b410678acd8
Matthew Jenner <matthewjenner@hotmail.com>
2023-11-21 13:14:26 +0000

    remove items from cart

--- comfy-store/starter/src/cart/setupCart.js
+++ comfy-store/starter/src/cart/setupCart.js
@@ -60,6 +60,9 @@ function displayCartItemsDOM(){
     addToCartDOM(cartItem)
   })
 }
+function removeItem(id) {
+  cart = cart.filter((cartItem) => cartItem.id !== id)
+}
 function increaseAmount(id) {
   let newAmount
   cart = cart.map((cartItem) => {
@@ -72,7 +75,22 @@ cartItem = {...cartItem, amount: newAmount}
   return newAmount
 }
 function setupCartFunctionality(){
-
+cartItemsDOM.addEventListener('click', function(e) {
+  const element = e.target
+  const parent = e.target.parentElement
+  const id = e.target.dataset.id
+  const parentID = e.target.parentElement.dataset.id
+// remove
+if(element.classList.contains('cart-item-remove-btn')) {
+  removeItem(id)
+  parent.parentElement.remove()
+}
+// increase
+// decrease
+  displayCartItemCount()
+  displayCartTotal()
+  setStorageItem('cart', cart)
+})
 }
 const init = () => {
 // display amount of cart items
